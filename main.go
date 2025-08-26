8c5b66420cd0f5e97815bb35d83e62d5eb694c86
ServersCamp <mail@serverscamp.com>
2025-08-26 17:01:50 +0300

    Prepare provider for release v0.1.0

--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
     "context"
-
     "github.com/hashicorp/terraform-plugin-framework/providerserver"
     "github.com/hashicorp/terraform-plugin-log/tflog"
-
-    "scamp/internal/provider"
+    "github.com/serverscamp/terraform-provider-scamp/internal/provider"
 )
 
 var version = "0.1.0"
@@ -15,6 +13,6 @@ func main() {
     ctx := context.Background()
     tflog.Info(ctx, "Starting SCAMP Terraform Provider", map[string]any{"version": version})
     providerserver.Serve(ctx, provider.New, providerserver.ServeOpts{
-        Address: "registry.terraform.io/you/scamp",
+        Address: "registry.terraform.io/serverscamp/scamp",
     })
 }
